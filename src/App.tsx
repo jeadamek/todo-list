f26c90c5d7b49585e4ee2fda2df9c59f38dfc838
Jessica Adamek <jessica.g.adamek@gmail.com>
2025-11-04 22:19:02 -0300

    icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import Text from "./components/text";
+import TrashIcon from './assets/icons/trash.svg?react';
+import CheckIcon from './assets/icons/check.svg?react';
+import PencilIcon from './assets/icons/pencil.svg?react';
+import PlusIcon from './assets/icons/plus.svg?react';
+import SpinnerIcon from './assets/icons/spinner.svg?react';
+import Icon from "./components/icon";
 
 export default function App() {
   return (
-    <Text variant={"body-md"}>
-      Hello, world!
-    </Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant={"body-md"}>
+          Hello, world!
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base"/>
+        <Icon svg={PencilIcon} className="fill-green-base"/>
+        <Icon svg={CheckIcon} className="fill-green-base"/>
+        <Icon svg={PlusIcon} className="fill-green-base"/>
+        <Icon svg={SpinnerIcon} animate />
+      </div>
+    </div>
   )
 }
