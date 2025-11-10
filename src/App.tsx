72a0a46fd706319c1f55ba4b20a9944b67677c8d
Jessica Adamek <jessica.g.adamek@gmail.com>
2025-11-09 22:16:06 -0300

    container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,54 @@
 import Text from "./components/text";
-import TrashIcon from './assets/icons/trash.svg?react';
-import CheckIcon from './assets/icons/check.svg?react';
-import PencilIcon from './assets/icons/pencil.svg?react';
-import PlusIcon from './assets/icons/plus.svg?react';
-import SpinnerIcon from './assets/icons/spinner.svg?react';
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
 import InputText from "./components/Input-text";
 import InputCheckbox from "./components/input-checkbox";
 import Card from "./components/card";
+import Container from "./components/container";
 
 export default function App() {
   return (
-    <div className="grid gap-10">
-      <div className="flex flex-col gap-1">
-        <Text variant={"body-md"}>
-          Hello, world!
-        </Text>
-      </div>
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-1">
+          <Text variant={"body-md"}>Hello, world!</Text>
+        </div>
 
-      <div className="flex gap-1">
-        <Icon svg={TrashIcon} className="fill-green-base"/>
-        <Icon svg={PencilIcon} className="fill-green-base"/>
-        <Icon svg={CheckIcon} className="fill-green-base"/>
-        <Icon svg={PlusIcon} className="fill-green-base"/>
-        <Icon svg={SpinnerIcon} animate />
-      </div>
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-green-base" />
+          <Icon svg={PencilIcon} className="fill-green-base" />
+          <Icon svg={CheckIcon} className="fill-green-base" />
+          <Icon svg={PlusIcon} className="fill-green-base" />
+          <Icon svg={SpinnerIcon} animate />
+        </div>
 
-      <div>
-        <Badge variant={"secondary"}>5</Badge>
-        <Badge variant={"primary"}>2 de 5</Badge>
-      </div>
+        <div>
+          <Badge variant={"secondary"}>5</Badge>
+          <Badge variant={"primary"}>2 de 5</Badge>
+        </div>
 
-      <div>
-        <Button icon={PlusIcon}>
-          Nova Tarefa
-        </Button>
-      </div>
+        <div>
+          <Button icon={PlusIcon}>Nova Tarefa</Button>
+        </div>
 
-      <div>
-        <InputText />
-      </div>
+        <div>
+          <InputText />
+        </div>
 
-      <div>
-        <InputCheckbox />
-      </div>
+        <div>
+          <InputCheckbox />
+        </div>
 
-      <div>
-        <Card size={"md"}> Hello World</Card>
+        <div>
+          <Card size={"md"}> Hello World</Card>
+        </div>
       </div>
-    </div>
-  )
+    </Container>
+  );
 }
